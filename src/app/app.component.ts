d33523d085a3fbfe11600b6ff000ee1d7975219c
homo-digitalis <secretary@homo-digitalis.org>
2019-06-19 13:53:16 +0200

    whatsapp links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,24 +19,26 @@ export class AppComponent implements OnInit {
   public ngOnInit() {
     this.activatedRoute.queryParams.subscribe((queryParams) => {
       switch (queryParams.day) {
-        case 'monday': window.open('https://chat.whatsapp.com/CBU73BL7SptJDf3p1R5mMq');
+        case 'monday':
+          document.location.href = 'https://chat.whatsapp.com/CBU73BL7SptJDf3p1R5mMq';
           break;
-        case 'tuesday': window.open('https://chat.whatsapp.com/IDQQisk7vUzAHHgF1UM7To');
+        case 'tuesday':
+          document.location.href = 'https://chat.whatsapp.com/IDQQisk7vUzAHHgF1UM7To';
           break;
         case 'wednesday':
-          console.log("hmmmmmm")
-          //window.open('https://chat.whatsapp.com/Gvz4aq6Jvgq6elxNRKnbc4');
-          //this.router.navigate(['https://chat.whatsapp.com/Gvz4aq6Jvgq6elxNRKnbc4'])
           document.location.href = 'https://chat.whatsapp.com/Gvz4aq6Jvgq6elxNRKnbc4';
-
           break;
-        case 'thursday': window.open('https://chat.whatsapp.com/GhqSHLSGMF7L06DnsWPxYr');
+        case 'thursday':
+          document.location.href = 'https://chat.whatsapp.com/GhqSHLSGMF7L06DnsWPxYr';
           break;
-        case 'friday': window.open('https://chat.whatsapp.com/Epx3YNiXdrFC0dYHF6BnA8');
+        case 'friday':
+          document.location.href = 'https://chat.whatsapp.com/Epx3YNiXdrFC0dYHF6BnA8';
           break;
-        case 'saturday': window.open('https://chat.whatsapp.com/Is6qY0QZ0WHLA83yoDVJWp');
+        case 'saturday':
+          document.location.href = 'https://chat.whatsapp.com/Is6qY0QZ0WHLA83yoDVJWp';
           break;
-        case 'sunday': window.open('https://chat.whatsapp.com/KGe9ZxqVTg77DNvORCONol');
+        case 'sunday':
+          document.location.href = 'https://chat.whatsapp.com/KGe9ZxqVTg77DNvORCONol';
           break;
       }
       console.log(queryParams.day);
